ebe005c40daeafdf6fc2d0e664f0e4880d24071e
Brandon De Leon <brandondeleon.615@gmail.com>
2021-10-27 10:49:59 -0500

    added create tag component

---
+++ src/components/tags/CreateTag.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+
+export const CreateTag = () =>{
+return(
+    <>
+    <div>
+        <form>
+            <h2>Create a Tag</h2>
+        </form>
+    </div>
+    </>
+)
+
+}
