4ef5267649cac079e1f406430a1ecfd7b29f34c6
Brandon De Leon <brandondeleon.615@gmail.com>
2021-10-27 10:56:29 -0500

    add form in create tag component

--- src/components/tags/CreateTag.js
+++ src/components/tags/CreateTag.js
@@ -8,6 +8,8 @@ return(
     <div>
         <form>
             <h2>Create a Tag</h2>
+            <label htmlFor="description">title</label>
+            <input  type="text" name="title" className="form-control" placeholder=" Tag Title" required autoFocus />
         </form>
     </div>
     </>
